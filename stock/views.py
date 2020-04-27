628d0ddb8b787caf465e05bb822d7df829419777
Soontorn Choosenpom <soontorn.cho@mail.pbru.ac.th>
2020-04-27 19:02:24 +0700

    commit2

--- stock/views.py
+++ stock/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
 from .models import Product
 
-#from django.http import HttpResponse
+# from django.http import HttpResponse
+
 
 # Create your views here.
-
-
 def index(request):
     products = Product.objects.all()
-
-    #html = "<html><body>Home page</body></html>"
-    # return HttpResponse(html)
-    return render(request, 'frontend/index.html', {'product':products})
+    # html = "<html><body>Home page</body></html>"
+    #return HttpResponse(html)
+    return render(request, 'frontend/index.html', {'products': products})
 
 
 def about(request):
